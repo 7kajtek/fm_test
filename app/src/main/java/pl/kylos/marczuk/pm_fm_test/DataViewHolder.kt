d68da3e118ca35d4fa62defb7e0cf8976badf717
Pawel Marczuk <pawel.marczuk@wp.pl>
2021-11-30 00:25:55 +0100

    Add picasso

--- app/src/main/java/pl/kylos/marczuk/pm_fm_test/DataViewHolder.kt
+++ app/src/main/java/pl/kylos/marczuk/pm_fm_test/DataViewHolder.kt
@@ -2,8 +2,10 @@ package pl.kylos.marczuk.pm_fm_test
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import pl.kylos.marczuk.pm_fm_test.repository.Data
 
 class DataViewHolder(parentView: ViewGroup) :
@@ -16,5 +18,7 @@ class DataViewHolder(parentView: ViewGroup) :
         itemView.findViewById<TextView>(R.id.item_title).text = item.title
         itemView.findViewById<TextView>(R.id.item_date).text = item.modificationDate
         itemView.findViewById<TextView>(R.id.item_description).text = item.description
+        itemView.findViewById<ImageView>(R.id.item_image).setImageResource(0)
+        Picasso.get().load(item.imageUrl).into(itemView.findViewById<ImageView>(R.id.item_image));
     }
 }
