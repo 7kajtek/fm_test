d6cbf89f41a565df22e6a251fa4340c11dd7074a
Pawel Marczuk <pawel.marczuk@wp.pl>
2021-11-30 00:59:18 +0100

    Format data

--- app/src/main/java/pl/kylos/marczuk/pm_fm_test/DataViewHolder.kt
+++ app/src/main/java/pl/kylos/marczuk/pm_fm_test/DataViewHolder.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import pl.kylos.marczuk.pm_fm_test.repository.Data
+import java.text.DateFormat
 
 class DataViewHolder(parentView: ViewGroup) :
     RecyclerView.ViewHolder(
@@ -15,8 +16,12 @@ class DataViewHolder(parentView: ViewGroup) :
     ) {
 
     fun bind(item: Data) {
+        val dateFormat: DateFormat =
+            android.text.format.DateFormat.getDateFormat(itemView.context.applicationContext);
+
         itemView.findViewById<TextView>(R.id.item_title).text = item.title
-        itemView.findViewById<TextView>(R.id.item_date).text = item.modificationDate
+        itemView.findViewById<TextView>(R.id.item_date).text =
+            item.getModificationDate()?.let { dateFormat.format(it) } ?: ""
         itemView.findViewById<TextView>(R.id.item_description).text = item.description
         itemView.findViewById<ImageView>(R.id.item_image).setImageResource(0)
         Picasso.get().load(item.imageUrl).into(itemView.findViewById<ImageView>(R.id.item_image));
--- app/src/main/java/pl/kylos/marczuk/pm_fm_test/repository/Data.kt
+++ app/src/main/java/pl/kylos/marczuk/pm_fm_test/repository/Data.kt
@@ -1,10 +1,23 @@
 package pl.kylos.marczuk.pm_fm_test.repository
 
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
 data class Data(
-    val id:Int,
+    val id: Int,
     val description: String,
     val imageUrl: String,
-    val modificationDate: String,
+    private val modificationDate: String,
     val title: String,
     val webUrl: String
-)
+) {
+    fun getModificationDate(): Date? {
+        try {
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+            return dateFormat.parse(modificationDate)
+        } catch (e: ParseException) {
+            return null;
+        }
+    }
+}
